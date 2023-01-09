1e96c14a4c9665b7af108f27548c5c50034efbfc
Simone Carriero <simonecarriero95@hotmail.com>
2023-01-09 09:06:15 +0100

    Add assets, style and components
    Added Main and Navbar components and logic to toggle for dark/light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import './style.css';
+
 const App = () => {
-  return <div></div>;
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((currState) => !currState);
+  };
+  return (
+    <div className='container'>
+      <Navbar
+        darkMode={darkMode}
+        toggleDarkMode={toggleDarkMode}
+      />
+      <Main darkMode={darkMode} />
+    </div>
+  );
 };
 
 export default App;
